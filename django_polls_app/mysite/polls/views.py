d64b293612d5156d862f68b705c6ca7f76ec53a5
Abhishek Kumar Roy <thunderoy013@gmail.com>
2017-02-03 00:18:53 +0530

    added some views

--- django_polls_app/mysite/polls/views.py
+++ django_polls_app/mysite/polls/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.http import HttpResponse
+
+def index(request):
+	return HttpResponse("This is the INDEX page.")
+
+def detail(request, question_id):
+	return HttpResponse("This is the DETAIL page of question %s." % question_id)
+
+def results(request, question_id):
+	return HttpResponse("This is the RESULTS page of question %s." % question_id)
+
+def vote(request, question_id):
+	return HttpResponse("This is the VOTE page of question %s." % question_id)
